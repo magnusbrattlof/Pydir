20be9a941c898ed3d784cb232d9cd20f02bd5bea
Magnus <brattlof.magnus@gmail.com>
2017-10-25 21:17:31 +0200

    Added path config and pydir

---
+++ pydir.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import configparser
+import time
+import sys
+import os
+
+# Path initalization
+with open('path.config', 'r', encoding='utf-8') as f:
+    PATHS = [path.replace('\n', '') for path in f]
+
+def main():
+    query = sys.argv[1]
+    results = search(query)
+
+    if len(results) == 0:
+        print('Did not find {}'.format(query))
+    elif len(results) > 1:
+        for index, path in enumerate(results):
+            print('[{}] {}'.format(index, path))
+        menu(results[int(input('Select: '))])
+    else:
+        menu(results[0])
+
+def menu(path):
+    os.system("start cd {}".format(path))
+
+def search(query):
+    results = []
+    
+    for path in PATHS:
+        for subdir, rootdir, _ in os.walk(path):
+            if query.lower() in [rd.lower() for rd in rootdir]:
+                results.append('{}\\{}'.format(subdir, query))
+    return results
+
+if __name__ == '__main__':
+    main()
